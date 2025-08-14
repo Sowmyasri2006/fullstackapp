ad7d66b6639e025465e54315b31e82f889cd5632
Sowmyasri2006 <sowmyasri849@gmail.com>
2025-08-14 12:21:10 +0530

    Update App.jsx

--- crud_frontend/crud_frontend-main/src/App.jsx
+++ crud_frontend/crud_frontend-main/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   const handleChange = (e) => {
     setProduct({ ...product, [e.target.name]: e.target.value });
   };
-const BASE_URL = 'http://172.31.0.0/16/springapp1';
+const BASE_URL = 'http://18.234.29.129/springapp1';
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
